be0d1b91805f31efc29c6b391aa0d1ce10aa69d3
Bokeher <bokeher47@gmail.com>
2021-02-09 09:44:02 +0100

    1.8

--- 1.8/Kalkulator.java
+++ 1.8/Kalkulator.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 class Kalkulator{
     public static void main (String[] args){
@@ -14,25 +17,44 @@ class Kalkulator{
                     System.out.println();
                 }
                 if(!chosenFigure.toLowerCase().equals("exit")){
-                    String drawedFigure = "\n";
+                    String drawnFigure = "";
                     int area;
                     int perimeter;
+
                     if(chosenFigure.equals("1")){
                         System.out.print("Podaj dlugosc boku: ");
                         int length = sc.nextInt();
-                        drawedFigure += drawFigure(length, length);
+
+                        drawnFigure = drawFigure(length, length);
                         area = length*length;
                         perimeter = 4*length;
+
+                        try{
+                            FileWriter squareFileWr = new FileWriter("kwadrat.txt", true);
+                            squareFileWr.write(drawnFigure+"\n"+length+";"+area+";"+perimeter+"\n");
+                            squareFileWr.close();
+                        }catch(Exception e){
+                            System.out.println(e.toString());
+                        }
                     }else{
-                        System.out.print("Podaj wysokosc boku: ");
-                        int height = sc.nextInt();
                         System.out.print("Podaj szerokosc: ");
                         int width = sc.nextInt();
-                        drawedFigure += drawFigure(width, height);
+                        System.out.print("Podaj wysokosc: ");
+                        int height = sc.nextInt();
+
+                        drawnFigure = drawFigure(width, height);
                         area = height*width;
-                        perimeter = 2*(height+width);               
+                        perimeter = 2*(height+width);
+
+                        try{
+                            FileWriter rectangleFileWr = new FileWriter("prostokat.txt", true);   
+                            rectangleFileWr.write(""+drawnFigure+"\n"+width+";"+height+";"+area+";"+perimeter+"\n");
+                            rectangleFileWr.close();  
+                        }catch(Exception e){
+                            System.out.println(e.toString());
+                        }
                     }
-                    System.out.println(drawedFigure+"\nPole wynosi: "+area+"\nObwod wynosi: "+perimeter+"\n\n");
+                    System.out.println("\n"+drawnFigure+"\n\nPole wynosi: "+area+"\nObwod wynosi: "+perimeter+"\n");
                 }else a=0;
             }else a=0;
         }
@@ -40,12 +62,21 @@ class Kalkulator{
 
     public static String drawFigure(int x, int y){
         String figure = "";
-        for(int i=0; i<y-1; i++){
-            if(i==0) for(int j=0; j<x; j++) figure+="*";
-            figure += "\n*";
-            if(i<y-2)for(int j=0; j<x-2; j++) figure+=" ";
-            if(i<y-2) figure += "*";
-            else for(int j=0; j<x-1; j++) figure+="*";
+        if(x==1 && y==1) return "*";
+        else if(x<2 || y<2){
+            for(int i=0; i<y; i++){
+                for(int j=0; j<x; j++) figure += "*";
+                figure+="\n";
+            }
+        }else{
+            for(int i=0; i<y-1; i++){
+                if(i==0) for(int j=0; j<x; j++) figure+="*";
+                figure += "\n*";
+                if(i<y-2){
+                    for(int j=0; j<x-2; j++) figure+=" ";
+                    figure += "*";
+                }else for(int j=0; j<x-1; j++) figure+="*";
+            }
         }
         return figure;
     }
@@ -58,6 +89,8 @@ class Kalkulator{
         System.out.println("2 - Prostokat");
         System.out.println("EXIT - Wyjscie");
         System.out.print("Wybor: ");
-        return sc.next();
+        String chosen = sc.next();
+        System.out.println("-=-=-=-=-=-=-=-=-=");
+        return chosen;
     }
 }

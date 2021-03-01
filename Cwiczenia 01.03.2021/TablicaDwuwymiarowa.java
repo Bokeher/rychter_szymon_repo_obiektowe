9f2e5dc946727638fbcfb6316394662412b07d96
Bokeher <bokeher47@gmail.com>
2021-03-01 09:25:21 +0100

    wyswietlanie tablicy dwuwymiarowej

--- Cwiczenia 01.03.2021/TablicaDwuwymiarowa.java
+++ Cwiczenia 01.03.2021/TablicaDwuwymiarowa.java
@@ -2,6 +2,19 @@ class TablicaDwuwymiarowa{
     public static void main(String[] args){
         int [] arr = new int[5];
         arr[0] = 40;
-        arr[1] = "Jeden";
+        //for(int i=0; i<arr.length; i++) System.out.println(arr[i]);
+
+        int [][] arr2d = new int [5][2];
+        arr2d[0][1] = 50;
+        arr2d[2][1] = -200;
+        arr2d[4][0] = 444;
+
+        for(int i=0; i<arr2d.length; i++){
+            for(int j=0; j<arr2d[i].length; j++) 
+                System.out.print(arr2d[i][j]+" ");
+            System.out.println("");
+        } 
+            
+
     }
 } 

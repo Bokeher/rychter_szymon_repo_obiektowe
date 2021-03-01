50875bbf6c70bba3e5094b5114665b6421726ab4
Bokeher <bokeher47@gmail.com>
2021-03-01 09:30:02 +0100

    Przeniesienie do osobnych funkcji

--- Cwiczenia 01.03.2021/TablicaDwuwymiarowa.java
+++ Cwiczenia 01.03.2021/TablicaDwuwymiarowa.java
@@ -1,9 +1,9 @@
 class TablicaDwuwymiarowa{
     public static void main(String[] args){
-        int [] arr = new int[5];
-        arr[0] = 40;
-        //for(int i=0; i<arr.length; i++) System.out.println(arr[i]);
+        
+    }
 
+    private static void arr2dFirst(){
         int [][] arr2d = new int [5][2];
         arr2d[0][1] = 50;
         arr2d[2][1] = -200;
@@ -14,7 +14,11 @@ class TablicaDwuwymiarowa{
                 System.out.print(arr2d[i][j]+" ");
             System.out.println("");
         } 
-            
+    }
 
+    private static void przypomnienieTablicJednowymiarowych(){
+        int [] arr = new int[5];
+        arr[0] = 40;
+        for(int i=0; i<arr.length; i++) System.out.println(arr[i]);
     }
 } 

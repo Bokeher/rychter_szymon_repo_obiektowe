59a2f2a77a49f41b4bbb49e88f783dee1ba01cf8
Bokeher <bokeher47@gmail.com>
2021-03-02 10:33:41 +0100

    Wyswietalnie z 0 na poczatku

--- Cwiczenia 02.03.2020/Zad.java
+++ Cwiczenia 02.03.2020/Zad.java
@@ -13,8 +13,8 @@ class Zad{
 
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[i].length; j++){
-                System.out.print(arr[i][j]+"\t");
-                //System.out.print(String.format("%d ", arr[i][j]));
+                //System.out.print(arr[i][j]+"\t");
+                System.out.print(String.format("%03d ", arr[i][j]));
             }
             System.out.println();
         }

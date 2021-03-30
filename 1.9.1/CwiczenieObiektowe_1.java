ce31ce55cf0035c5740dbdfc3edf99c014dee5c0
Bokeher <bokeher47@gmail.com>
2021-03-30 09:17:54 +0200

    2 Zadanie

--- 1.9.1/CwiczenieObiektowe_1.java
+++ 1.9.1/CwiczenieObiektowe_1.java
@@ -2,5 +2,8 @@ class CwiczenieObiektowe_1{
     public static void main(String[] args){
         MojeDane md = new MojeDane("Szymon", "Rychter", "2PTN");
         System.out.println("Imie: "+md.imie+"\nNazwisko: "+md.nazwisko+"\nKlasa: "+md.klasa);
+
+        Logowanie log = new Logowanie();
+        System.out.println("\n"+log.toString());
     }
 }
---
+++ 1.9.1/Logowanie.java
@@ -0,0 +1,9 @@
+class Logowanie{
+    private String login = "commit";
+    private String haslo = "12345";
+
+    @Override
+    public String toString(){
+        return "Login: "+login+"  Password: "+haslo;
+    }
+}

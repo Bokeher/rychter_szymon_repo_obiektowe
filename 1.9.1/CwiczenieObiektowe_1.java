64f16aa4c4c1f74ea001cbf927b935a06df84efe
Bokeher <bokeher47@gmail.com>
2021-03-30 09:00:06 +0200

    1 commit

---
+++ 1.9.1/CwiczenieObiektowe_1.java
@@ -0,0 +1,5 @@
+class CwiczenieObiekotwe_1{
+    public static void main(String[] args){
+        MojeDane md = new MojeDane("Szymon", "Rychter", "2PTN");
+    }
+}
---
+++ 1.9.1/MojeDane.java
@@ -0,0 +1,11 @@
+class MojeDane{
+    public String imie = "";
+    public String nazwisko = "";
+    public String klasa = ""; 
+
+    public void MojeDane(String imie, String nazwisko, String klasa){
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.klasa = klasa;
+    }
+}
